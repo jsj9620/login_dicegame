2a7f9b1d9b6d1bf5af704a165fcf4ce58ef0bb6c
SEUNGJUN JEONG <jsj9620@naver.com>
2022-07-09 23:02:47 +0900

    create login function and dice game activity

---
+++ lib/dice.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dice extends StatefulWidget {
+  const Dice({Key? key}) : super(key: key);
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
