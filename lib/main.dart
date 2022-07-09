01dd907c0489b497ace6a320403bcc6332638991
SEUNGJUN JEONG <jsj9620@naver.com>
2022-07-09 22:42:31 +0900

    create login form

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
   const LogIn({Key? key}) : super(key: key);
 
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +40,64 @@ class LogIn extends StatelessWidget {
               onPressed: (){}
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child: Image(
+                image: AssetImage('assets/chef.gif'),
+                width: 170.0,
+                height: 190.0,
+              ),
+            ),
+            Form(child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15.0)
+                  )
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(40.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "dice"'
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Enter Password'
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      ButtonTheme(
+                        minWidth: 100.0,
+                          height: 50.0,
+                          child: ElevatedButton(
+                              style : ElevatedButton.styleFrom(
+                                primary: Colors.orangeAccent
+                              ),
+                              onPressed: (){},
+                              child: Icon(Icons.arrow_forward, color: Colors.white, size: 35.0),
+                          )
+                      ),
+                    ],
+                  ),
+                ))
+            ),
+          ],
+        ),
       ),
     );
   }
