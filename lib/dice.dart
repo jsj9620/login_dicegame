5e8a63703d1007c2603dffa517db8496d4bf7c47
SEUNGJUN JEONG <jsj9620@naver.com>
2022-07-09 23:29:22 +0900

    designed dice activity

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Dice extends StatefulWidget {
+class Dice extends StatelessWidget {
   const Dice({Key? key}) : super(key: key);
 
   @override
-  State<Dice> createState() => _DiceState();
-}
-
-class _DiceState extends State<Dice> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text('Dice Game'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                children: [
+                  Expanded(child: Image.asset('assets/dice1.png')),
+                  SizedBox(width: 20.0),
+                  Expanded(child: Image.asset('assets/dice2.png')),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.orangeAccent,
+                  minimumSize: Size(100, 50)
+                ),
+                child: Icon(Icons.play_arrow, color: Colors.white, size: 50.0,)
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
