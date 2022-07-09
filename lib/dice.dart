d1efc4eed71ab0e26913df5ebc3101841e41bca6
SEUNGJUN JEONG <jsj9620@naver.com>
2022-07-10 00:11:54 +0900

    [FINAL] add roll dice func and count scores

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
   const Dice({Key? key}) : super(key: key);
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int leftScore = 0;
+  int rightDice = 2;
+  int rightScore = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,26 +31,56 @@ class Dice extends StatelessWidget {
               padding: const EdgeInsets.all(32.0),
               child: Row(
                 children: [
-                  Expanded(child: Image.asset('assets/dice1.png')),
+                  Expanded(child: Image.asset('assets/dice$leftDice.png')),
                   SizedBox(width: 20.0),
-                  Expanded(child: Image.asset('assets/dice2.png')),
+                  Expanded(child: Image.asset('assets/dice$rightDice.png')),
                 ],
               ),
             ),
-            SizedBox(
-              height: 60.0,
-            ),
-            ElevatedButton(
-                onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.orangeAccent,
-                  minimumSize: Size(100, 50)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    '$leftScore',
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                SizedBox(width: 170.0),
+                Text(
+                  '$rightScore',
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
                 ),
-                child: Icon(Icons.play_arrow, color: Colors.white, size: 50.0,)
+              ],
             ),
+            SizedBox(height: 30.0),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    leftDice = Random().nextInt(6) + 1;
+                    rightDice = Random().nextInt(6) + 1;
+                    leftScore += leftDice;
+                    rightScore += rightDice;
+                  });
+                  showToast("Left : [$leftDice], Right : [$rightDice]");
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.orangeAccent, minimumSize: Size(100, 50)),
+                child: Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 50.0,
+                )),
           ],
         ),
       ),
     );
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      textColor: Colors.black,
+      backgroundColor: Colors.orangeAccent,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM);
 }
